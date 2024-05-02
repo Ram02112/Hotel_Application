9524fcd8fd503943889cf2639f5d70ad714925a9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-02 16:46:01 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backend/models/Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -8,12 +8,17 @@ const bookingSchema = new Schema(
     date: { type: Date, required: true },
     time: { type: String, required: true },
     numberOfPeople: { type: Number, required: true },
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "customers",
+      required: true,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Booking = mongoose.model("bookings", bookingSchema);
+const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
