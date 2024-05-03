8cf7e130ae7624f033321a557dc357316c76138b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-03 10:37:37 +0530

    Changes to be committed: 	modified:   backend/utils/sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,23 +1,23 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "youremail@gmail.com", // generated ethereal user
-        pass: "abdcedfxyz"  // generated ethereal password
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "youremail@gmail.com",
+    pass: "abdcedfxyz",
+  },
 });
 
 let sendEmail = (emailTemplate) => {
-    transporter.sendMail(emailTemplate, (err, info) => {
-        if(err) {
-            console.log(err)
-        }else{
-            console.log('Email sent: ', info.response)
-        }
-    })
-}
+  transporter.sendMail(emailTemplate, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
+  });
+};
 
-module.exports = {sendEmail};
+module.exports = { sendEmail };
